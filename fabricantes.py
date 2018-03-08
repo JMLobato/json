fc2dd7f9dd091c45f32b6f667afe1eeb2d1a2443
JMLobato <derc8aid@gmail.com>
2018-03-08 23:16:34 +0100

    3º apartado terminado

--- fabricantes.py
+++ fabricantes.py
@@ -6,5 +6,13 @@ cont=0
 for actividad in datos:
 	print(actividad["name"])
 	cont+=1
+print()
 #2º Apartado
 print("Hay %d actividades"%cont)
+print()
+#3º Apartado
+fecha=input("Introduce una fecha separada por guiones y empezando por el año: ")
+for fechi in datos:
+	if str(fechi["date"][0])==str(fecha):
+		print(fechi["name"])
+print()
