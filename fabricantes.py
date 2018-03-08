2420a50b368db4f86ea320a94262b6ba52b19e76
JMLobato <derc8aid@gmail.com>
2018-03-08 23:03:12 +0100

    2º apartado terminado

--- fabricantes.py
+++ fabricantes.py
@@ -1,5 +1,10 @@
 import json
 with open("Fabricantes.json") as fichero:
 	datos=json.load(fichero)
+cont=0
+#1º Apartado
 for actividad in datos:
 	print(actividad["name"])
+	cont+=1
+#2º Apartado
+print("Hay %d actividades"%cont)
